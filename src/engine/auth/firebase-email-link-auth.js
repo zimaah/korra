b50588402e87ba7ac46c392397a7fb7fedf510d5
Guilherme Zimmermann <zimaah@gmail.com>
2023-04-13 01:00:03 +0200

    QA URL redirect

--- src/engine/auth/firebase-email-link-auth.js
+++ src/engine/auth/firebase-email-link-auth.js
@@ -4,7 +4,7 @@ import { app } from "../persistence/firebase";
 const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'http://localhost:8080',
+    url: 'https://korraaa-qa.web.app/',
     // This must be true.
     handleCodeInApp: true,
 };
